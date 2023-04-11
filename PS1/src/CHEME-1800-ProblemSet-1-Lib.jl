72f0732407c0fe73a11e08b99e97da0036c97af6
HaydenSpector <113485067+HaydenSpector@users.noreply.github.com>
2023-04-11 10:30:26 -0400

    Update CHEME-1800-ProblemSet-1-Lib.jl

--- PS1/src/CHEME-1800-ProblemSet-1-Lib.jl
+++ PS1/src/CHEME-1800-ProblemSet-1-Lib.jl
@@ -32,4 +32,27 @@ end
 Fill me in
 """
 function decrypt(encrypteddata::Dict{Int64,String})::String
+# initialize -
+number_of_chars = length(encrypteddata)
+inverse_encryptionkey_dict = Dict{String, Char}()
+plaintext = Vector{Char}()
+
+# build encryptionkey -
+encryption_model = _build(DNAEncryptionKey);
+encryptionkey = encryption_model.encryptionkey;
+
+# build the inverse_key -
+for (key, value) ∈ encryptionkey
+    inverse_encryptionkey_dict[value] = key
+end
+
+for i ∈ 0:(number_of_chars - 1)
+    
+    codon = encrypteddata[i]
+    value = inverse_encryptionkey_dict[codon]
+    push!(plaintext, value)
+end
+
+# return -
+return String(plaintext)
 end
