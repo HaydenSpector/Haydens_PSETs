3f7438032076a1bdeab9938e4666dbc8c071411f
HaydenSpector <113485067+HaydenSpector@users.noreply.github.com>
2023-05-09 11:37:59 -0400

    Update Compute.jl

--- PS5/src/Compute.jl
+++ PS5/src/Compute.jl
@@ -3,19 +3,38 @@
 TODO: Fill me in.
 """
 function build_data_matrix(data::DataFrame)::Array{Float64,2}
-    n = 100
-    m = 4
-    dm = zeros(n, m)
+   #initialize
+
+   number_of_repeats = 3;
+   
+    (n,m) = size(data);
 
     # Fill in the dynamic programming matrix
+    S_array = Array{Float64,1}();
+
     for i in 1:n  # this should avoid any errors
-        for j in 2:m
-            dm[i,j] = data[i,j] # 1-indexed
+        S_value - data[i,:S]
+        for j in 1:number_of_repeats
+            push!(S_array,(1/S_value))
         end
     end
 
-    return dm
+   # construct the data matrix -
+   X = Array{Float64,2}(undef, number_of_repeats*R, 2);
+   for i ∈ 1:number_of_repeats:(number_of_repeats*R)
+       for j ∈ i:(i+number_of_repeats-1)
+           
+           # get the S level -
+           S_value = S_array[j];
+           
+           # fill in the columns of the data matrix -
+           X[j,1] = 1.0;
+           X[j,2] = S_value
+       end
+   end
 
+   # return -
+   return X
 end
 
 """
@@ -23,13 +42,16 @@ end
 TODO: Fill me in.
 """
 function build_output_vector(data::DataFrame)::Array{Float64,1}
-    n = 100
-    ov = zeros(n)
+    number_of_repeats = 3
+    (n,m) = size(data);
+    Y_array = Array{Floar64,1}();
     for i in 1:n
-        ov[i, 1] = data[i, 1]
+        for j in number_of_repeats
+            Y_value = [i,1+j];
+        end
     end
 
-    return ov
+    return Y_array;
 end
 
 """
@@ -38,7 +60,10 @@ Fill me in.
 """
 function build_error_distribution(residuals::Array{Float64,1})::Normal
 
-    # initialize -
+    #initialize-
+    μ = 0.0;
+    σ = 0.0;
+    # compute
     μ = mean(residuals); # default value, replace with your value
     σ = std(residuals); # default value, replace with your value
 
